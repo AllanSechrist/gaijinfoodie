edda9a3a3f520372eb72e62747362b9e207914cd
Allan Sechrist <sechristallan@gmail.com>
2025-01-01 08:51:04 +0900

    Add new blog and new restaurant buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def button_data
+    case controller_name
+    when "blogs"
+      { text: "New Blog", path: new_blog_path }
+    when "restaurants"
+      { text: "New Restaurant", path: new_restaurant_path }
+    else
+      { text: "Home", path: root_path }
+    end
+  end
 end
