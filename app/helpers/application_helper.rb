4b5c21b8dabb3e5e8c36bc38666ebb68f3bdafc9
Allan Sechrist <sechristallan@gmail.com>
2025-01-05 14:05:58 +0900

    update nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,17 @@ module ApplicationHelper
     end
   end
 
+  def path_data
+    case controller_name
+    when "blogs"
+      { text: "Restaurants", path: restaurants_path }
+    when "restaurants"
+      { text: "Blogs", path: blogs_path }
+    else
+      { text: "Home", path: root_path }
+    end
+  end
+
   def toggle_auth_button
     if user_signed_in?
       link_to "Logout", destroy_user_session_path, data: { turbo_method: :delete }, class: 'btn btn-danger mx-2'
