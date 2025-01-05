49a20b5a3d4bc5623d63a2c84690c37a7e075271
Allan Sechrist <sechristallan@gmail.com>
2025-01-05 10:37:47 +0900

    Add drop down menus for ratings

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
   has_many :blogs
+  FOOD_RATINGS = ['😋', '😋😋', '😋😋😋', '😋😋😋😋', '😋😋😋😋😋'].freeze
+  SERVICE_RATINGS = ['👍', '👍👍', '👍👍👍', '👍👍👍👍', '👍👍👍👍👍'].freeze
+  PRICE_RATINGS = ['💸', '💸💸', '💸💸💸', '💸💸💸💸', '💸💸💸💸💸'].freeze
+
+  validates :name, :cuisine, presence: true
+  validates :food_rating, inclusion: { in: FOOD_RATINGS }
+  validates :service_rating, inclusion: { in: SERVICE_RATINGS }
+  validates :price_rating, inclusion: { in: PRICE_RATINGS }
 end
