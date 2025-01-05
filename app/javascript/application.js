fc504ce2859590383ea0a52a64305176437974a5
Allan Sechrist <sechristallan@gmail.com>
2025-01-05 14:12:00 +0900

    Fix logout issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-// import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
-
 // import { Application } from "stimulus";
 
 // const application = Application.start();
