904c923bec363955e5f8d0db88e6e93d2ad497ca
Allan Sechrist <sechristallan@gmail.com>
2025-01-05 16:34:52 +0900

    Add routes and update controller for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :blog_id)
+  end
 end
